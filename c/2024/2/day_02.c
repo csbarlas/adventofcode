654dd8c08a6ef2f8400439c102b6ff1dc11396c4
Chris Barlas <62313241+csbarlas@users.noreply.github.com>
2025-10-11 21:18:23 -0700

    day 2 pt 1 c solution

---
+++ c/2024/2/day_02.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define INIT_REPORT_LIST_SIZE 10
+
+struct ReportRow {
+    int levels[15];
+    size_t count;
+} typedef ReportRow;
+
+struct ReportList {
+    ReportRow* reports;
+    size_t count;
+    size_t capacity;
+} typedef ReportList;
+
+enum LEVEL_ORDER {
+    UNDEF,
+    ASC,
+    DEC
+};
+
+int readData(char* filename, ReportList* dest);
+int expandReport(ReportList* report, size_t newCapacity);
+void printReport(ReportList* report);
+void addLevelToReport(int level, int reportIndex, ReportList* list);
+int calculateSafeReports(ReportList* list);
+int isReportSafe(ReportRow* row);
+void printReportRow(ReportRow* row);
+
+int main(int argc, char** argv) {
+    /* verify args */
+    if (argc < 2) {
+        printf("Please specify input file\n");
+        return -1;
+    }
+
+    char* filename = argv[1];
+    ReportList data = {NULL, 0, 0};
+    if (expandReport(&data, INIT_REPORT_LIST_SIZE)) return -1;
+
+    /* read file */
+    if (readData(filename, &data)) {
+        printf("Error executing file operations on %s", filename);
+        return -1;
+    }
+    printReport(&data);
+    
+    /* determine safety of reports */
+    int numSafe = calculateSafeReports(&data);
+    printf("Number of safe reports: %d\n", numSafe);
+}
+
+int readData(char* filename, ReportList* dest) {
+    FILE* inputFile = fopen(filename, "r");
+    if (inputFile == NULL) {
+        return -1;
+    }
+
+    char lineBuffer[50];
+    while (fgets(lineBuffer, 50, inputFile) != NULL) {
+        int reportRowIndex = dest->count;
+        int bytes_read = 0;
+        int num = 0;
+        char* pointerInLine = lineBuffer;
+
+        while (sscanf(pointerInLine, "%d%n", &num, &bytes_read) == 1) {
+            addLevelToReport(num, reportRowIndex, dest);
+            //printReportRow(&dest->reports[reportRowIndex]);
+            pointerInLine += bytes_read;
+        }
+
+        dest->count += 1;
+    }
+
+    if (fclose(inputFile)) return -1;
+
+    return 0;
+}
+
+int expandReport(ReportList* report, size_t newCapacity) {
+    size_t oldCapacity = report->capacity;
+    printf("Expanding report from %lu to %lu\n", oldCapacity, newCapacity);
+
+    if (report->reports == NULL) {
+        printf("init\n");
+        /* initialize the struct */
+        ReportRow* rows = calloc(newCapacity, sizeof(ReportRow));
+        if (rows == NULL) return -1;
+
+        memset(rows, 0, sizeof(ReportRow) * newCapacity);
+        report->reports = rows;
+        report->capacity = newCapacity;
+    } else {
+        /* use realloc */
+        printf("realloc\n");
+        ReportRow* expanded = realloc(report->reports, newCapacity * sizeof(ReportRow));
+        if (expanded == NULL) return -1;
+
+        memset(expanded + (sizeof(ReportRow) * oldCapacity), 0, (newCapacity - oldCapacity) * sizeof(ReportRow));
+        report->reports = expanded;
+        report->capacity = newCapacity;
+    }
+    
+    printf("Report now has capacity: %lu from request capacity %lu\n", report->capacity, newCapacity);
+    return 0;
+}
+
+void printReport(ReportList* report) {
+    printf("Report count: %lu, capacity: %lu\n", report->count, report->capacity);
+
+    size_t i;
+    for (i = 0; i < report->count; i++) {
+        printf("Printing row %d of %d\n", i + 1, report->count);
+        ReportRow* row = &report->reports[i];
+        printReportRow(row);
+    }
+}
+
+void printReportRow(ReportRow* row) {
+    size_t j;
+    for (j = 0; j < row->count - 1; j++) {
+        printf("%d ", row->levels[j]);
+    }
+    printf("%d\n", row->levels[row->count - 1]);
+}
+
+void addLevelToReport(int level, int reportIndex, ReportList* list) {
+    if (list->count == list->capacity) {
+        if (expandReport(list, list->capacity * 2)) return;
+    }
+
+    ReportRow* row = &list->reports[reportIndex];
+    row->levels[row->count] = level;
+    row->count++;
+}
+
+int calculateSafeReports(ReportList* list) {
+    size_t i;
+    int accum = 0;
+    for (i = 0; i < list->count; i++) {
+        ReportRow* row = &list->reports[i];
+
+        if (isReportSafe(row) == 1) {
+            printf("%lu. Report SAFE\n", i);
+            accum += 1;
+        } else {
+            printf("%lu. Report UNSAFE\n", i);
+        }
+    }
+
+    return accum;
+}
+
+int isReportSafe(ReportRow* row) {
+    /* printf("Calculating safety on row with %d levels\n", row->count); */
+    printReportRow(row);
+    int isSafe = 1;
+    enum LEVEL_ORDER order = UNDEF;
+    size_t i;
+    for (i = 1; i < row->count; i++) {
+        if (order == UNDEF) {
+            int last = row->levels[i - 1];
+            int current = row->levels[i];
+
+            if (last == current) {
+                isSafe = 0;
+            } else if (last < current) {
+                order = ASC;
+            } else {
+                order = DEC;
+            }
+        }
+
+        int last = row->levels[i - 1];
+        int current = row->levels[i];
+
+        if (order == ASC && last > current) {
+            isSafe = 0;
+        } else if (order == DEC && current > last) {
+            isSafe = 0;
+        }
+
+        int diff = abs(current - last);
+        if (diff == 0 || diff > 3) {
+            isSafe = 0;
+        }
+
+        if (isSafe == 0) {
+            break;
+        }
+    }
+
+    return isSafe;
+}
