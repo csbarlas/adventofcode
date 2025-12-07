c31f34b24a58c2638bb4ffc78fc2798c010a797a
Chris Barlas <github.arrival558@passmail.com>
2025-12-06 21:36:32 -0500

    Add 2025 Swift project

--- c/2024/6/day_06.c
+++ c/2024/6/day_06.c
@@ -21,15 +21,20 @@ struct GuardData {
 } typedef GuardData;
 
 char map[MAX_MAP_DIMENSION][MAX_MAP_DIMENSION];
+char originalMap[MAX_MAP_DIMENSION][MAX_MAP_DIMENSION];
+char visitedObstaclesMap[MAX_MAP_DIMENSION][MAX_MAP_DIMENSION];
 GuardData guardData = {0, 0, 0, -1, 0}; /* initialize facing up */
+GuardData guardDataSafeCopy = {0, 0, 0, -1, 0};
 
 void run(char* filename);
 void importData(char* filename);
 MapEntity getEntityType(char input);
-void printMap();
+void printMap(char currentMap[MAX_MAP_DIMENSION][MAX_MAP_DIMENSION]);
 void runPartOne();
+void runPartTwo();
 void turnGuardRight();
 void moveGuardForwardOneStep();
+int runGuardSim(int isPartTwo);
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -45,6 +50,7 @@ int main(int argc, char** argv) {
 void run(char* filename) {
     importData(filename);
     runPartOne();
+    runPartTwo();
 }
 
 void importData(char* filename) {
@@ -74,6 +80,9 @@ void importData(char* filename) {
         }
     }
 
+    memcpy(originalMap, map, MAX_MAP_DIMENSION * MAX_MAP_DIMENSION);
+    guardDataSafeCopy = guardData;
+
     if (fclose(file) != 0) {
         printf("Error closing file!\n");
         return;
@@ -92,23 +101,56 @@ MapEntity getEntityType(char input) {
     }
 }
 
-void printMap() {
+void printMap(char currentMap[MAX_MAP_DIMENSION][MAX_MAP_DIMENSION]) {
     size_t y;
     for (y = 0; y < MAX_MAP_DIMENSION; y++) {
         size_t x;
         for (x = 0; x < MAX_MAP_DIMENSION; x++) {
-            MapEntity type = map[y][x];
+            MapEntity type = currentMap[y][x];
 
             if (type == TYPE_OUT_OF_BOUNDS) continue;
 
             printf("%d", type);
         }
 
-        if (map[y][0] != TYPE_OUT_OF_BOUNDS) printf("\n");
+        if (currentMap[y][0] != TYPE_OUT_OF_BOUNDS) printf("\n");
     }
 }
 
 void runPartOne() {
+    runGuardSim(0);
+    printf("The guard visited %lu distinct locations!\n", guardData.visitedTiles);
+}
+
+void runPartTwo() {
+    guardData = guardDataSafeCopy;
+    memcpy(map, originalMap, MAX_MAP_DIMENSION * MAX_MAP_DIMENSION);
+
+    int loops = 0;
+    size_t x, y;
+    for (y = 0; y < MAX_MAP_DIMENSION; y++) {
+        for (x = 0; x < MAX_MAP_DIMENSION; x++) {
+            MapEntity typeAtPointer = map[y][x];
+
+            if (typeAtPointer == TYPE_EMPTY) {
+                map[y][x] = TYPE_OBSTACLE;
+            } else {
+                continue;
+            }
+
+            loops += runGuardSim(1);
+
+            memcpy(map, originalMap, MAX_MAP_DIMENSION * MAX_MAP_DIMENSION);
+            memset(visitedObstaclesMap, 0, MAX_MAP_DIMENSION * MAX_MAP_DIMENSION);
+            guardData = guardDataSafeCopy;
+        }
+    }
+
+    printf("The guard had %d unique loops!\n", loops);
+}
+
+int runGuardSim(int isPartTwo) {
+    int didLoop = 0;
     MapEntity tileTypeAtGuardPos = TYPE_GUARD;
     while (tileTypeAtGuardPos != TYPE_OUT_OF_BOUNDS) {
         /* check what is in front of us */
@@ -117,6 +159,18 @@ void runPartOne() {
 
         MapEntity nextType = map[nextY][nextX];
         if (nextType == TYPE_OBSTACLE) {
+            if (isPartTwo) {
+                if (visitedObstaclesMap[nextY][nextX] == 1) {
+                    /* we have visited this obstable before and there is a loop */
+
+                    /* BUG: we need to track direction too*/
+                    didLoop = 1;
+                    break;
+                } else {
+                    visitedObstaclesMap[nextY][nextX] = 1;
+                }
+            }
+            
             /* if obstacle turn 90 deg to the right */
             turnGuardRight();
         } else {
@@ -128,7 +182,7 @@ void runPartOne() {
         tileTypeAtGuardPos = map[guardData.y][guardData.x];
     }
 
-    printf("The guard visited %lu distinct locations!\n", guardData.visitedTiles);
+    return didLoop;
 }
 
 void turnGuardRight() {
