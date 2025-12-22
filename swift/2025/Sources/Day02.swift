cdb206c5150b5ccaf3cb9d2e88fc7c8ab6ce5160
Chris Barlas <github.arrival558@passmail.com>
2025-12-21 19:51:05 -0500

    2025 day 2 pt 1 swift solution

---
+++ swift/2025/Sources/Day02.swift
@@ -0,0 +1,61 @@
+//
+//  Day02.swift
+//  AdventOfCode
+//
+//  Created by Chris Barlas on 12/20/25.
+//
+
+struct Day02: AdventDay {
+    private struct IdRange {
+        var lower, upper: String
+    }
+    
+    var data: String
+    
+    private var items: [IdRange] {
+        data.components(separatedBy: ",").compactMap {
+            let tmp = $0.components(separatedBy: "-")
+            if let lower = tmp.first, let upper = tmp.last {
+                return IdRange(lower: lower, upper: upper)
+            }
+            
+            return nil
+        }
+    }
+    
+    func part1() async throws -> Int {
+        var sum = 0
+        for range in items {
+            print("\(range.lower) to \(range.upper)")
+            if let lower = Int(range.lower.trimmingCharacters(in: .whitespacesAndNewlines)),
+               let upper = Int(range.upper.trimmingCharacters(in: .whitespacesAndNewlines)) {
+                for number in lower...upper {
+                    if isInvalid(number) {
+                        print("invalid: \(number)")
+                        sum += number
+                    }
+                }
+            }
+        }
+        return sum
+    }
+    
+    func part2() async throws -> UInt128 {
+        return 0
+    }
+    
+    private func isInvalid(_ id: Int) -> Bool {
+        let id = String(id)
+        for i in id.indices.prefix(id.count / 2) {
+            let pattern = id[...i]
+            let matches = id.ranges(of: pattern)
+            // first part tests for 2 matches (constraint #1)
+            // second part tests that the 2 matches comprise the whole string
+            if matches.count == 2 && (matches.count * pattern.count == id.count) {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
