f6b1eb7a1da6a817e3e9114ddef8b26018e6c330
Chris Barlas <github.arrival558@passmail.com>
2025-12-24 09:51:06 -0600

    2025 day 2 pt 2 swift solution

--- swift/2025/Sources/Day02.swift
+++ swift/2025/Sources/Day02.swift
@@ -24,14 +24,20 @@ struct Day02: AdventDay {
     }
     
     func part1() async throws -> Int {
+        invalidIdAggregator(isInvalid: isInvalidPartOne(_:))
+    }
+    
+    func part2() async throws -> Int {
+        invalidIdAggregator(isInvalid: isInvalidPartTwo(_:))
+    }
+    
+    private func invalidIdAggregator(isInvalid: (Int) -> Bool) -> Int {
         var sum = 0
         for range in items {
-            print("\(range.lower) to \(range.upper)")
             if let lower = Int(range.lower.trimmingCharacters(in: .whitespacesAndNewlines)),
                let upper = Int(range.upper.trimmingCharacters(in: .whitespacesAndNewlines)) {
                 for number in lower...upper {
                     if isInvalid(number) {
-                        print("invalid: \(number)")
                         sum += number
                     }
                 }
@@ -40,11 +46,7 @@ struct Day02: AdventDay {
         return sum
     }
     
-    func part2() async throws -> UInt128 {
-        return 0
-    }
-    
-    private func isInvalid(_ id: Int) -> Bool {
+    private func isInvalidPartOne(_ id: Int) -> Bool {
         let id = String(id)
         for i in id.indices.prefix(id.count / 2) {
             let pattern = id[...i]
@@ -52,6 +54,21 @@ struct Day02: AdventDay {
             // first part tests for 2 matches (constraint #1)
             // second part tests that the 2 matches comprise the whole string
             if matches.count == 2 && (matches.count * pattern.count == id.count) {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    private func isInvalidPartTwo(_ id: Int) -> Bool {
+        let id = String(id)
+        for i in id.indices.prefix(id.count / 2) {
+            let pattern = id[...i]
+            let matches = id.ranges(of: pattern)
+            // first part tests for at least 2 matches (constraint #1)
+            // second part tests that the matches comprise the whole string
+            if matches.count >= 2 && (matches.count * pattern.count == id.count) {
                 return true
             }
         }
